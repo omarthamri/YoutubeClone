945d21faadb4a37b101db07562b3d3dc65a5de3b
Omar Thamri <omarthamri4@gmail.com>
2019-06-27 18:38:34 +0100

    #0009 add slide in menu with nice animation

--- YoutubeClone/Controllers/SettingsLauncher.swift
+++ YoutubeClone/Controllers/SettingsLauncher.swift
@@ -11,6 +11,12 @@ import UIKit
 class SettingsLauncher: NSObject {
     
     let blackView = UIView()
+    let collectionView: UICollectionView = {
+       let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = UIColor.white
+        return cv
+    }()
     
     @objc func showSettings () {
         if let window = UIApplication.shared.keyWindow {
@@ -18,11 +24,15 @@ class SettingsLauncher: NSObject {
             blackView.backgroundColor = UIColor.init(white: 0, alpha: 0.5)
             blackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleDismiss)))
             window.addSubview(blackView)
+            window.addSubview(collectionView)
+            let y = window.frame.height - 200
+            collectionView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: 200)
             blackView.frame = window.frame
             blackView.alpha = 0
-            UIView.animate(withDuration: 0.5, animations: {
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
                 self.blackView.alpha = 1
-            })
+                self.collectionView.frame = CGRect(x: 0, y: y, width: window.frame.width, height: 200)
+            }, completion: nil)
         }
         
     }
@@ -31,6 +41,9 @@ class SettingsLauncher: NSObject {
         
         UIView.animate(withDuration: 0.5, animations: {
             self.blackView.alpha = 0
+            if let window = UIApplication.shared.keyWindow {
+                self.collectionView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: 200)
+            }
         })
     }
     
